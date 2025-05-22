465659a7412530aed05d83986ac299aa177221b7
ahmadtahirkamboh <ahmadtahir4235@gmail.com>
2025-05-22 04:52:32 -0700

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,19 @@
+import json
+import urllib.parse
+import boto3
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        print("CONTENT TYPE: " + response['ContentType'])
+        return response['ContentType']
+    except Exception as e:
+        print(e)
+        print(f"Error getting object {key} from bucket {bucket}.")
+        raise e
